a78a9a27c66c9900f3363af09511f7dc5612cd23
Clément NONN <clement.nonn@gmail.com>
2017-12-26 14:48:11 +0100

    creation of the xcode project

---
+++ Weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Clément NONN on 26/12/2017.
+//  Copyright © 2017 Clément NONN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
