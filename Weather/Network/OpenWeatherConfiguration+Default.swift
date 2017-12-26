7f215e6a8cdae7d2d69243f6ec8621a265f28f04
Clément NONN <clement.nonn@gmail.com>
2017-12-26 18:37:15 +0100

    add default conf for OpenWeather, with hardcoded Lyon and °C

---
+++ Weather/Network/OpenWeatherConfiguration+Default.swift
@@ -0,0 +1,19 @@
+//
+//  OpenWeatherConfiguration+Default.swift
+//  Weather
+//
+//  Created by Clément NONN on 26/12/2017.
+//  Copyright © 2017 Clément NONN. All rights reserved.
+//
+
+import Foundation
+
+extension OpenWeatherConfiguration {
+    static var `default`: OpenWeatherConfiguration {
+        return OpenWeatherConfiguration(
+            location: .id(6454573),
+            unit: .metric,
+            apiKey: "APIKEY"
+        )
+    }
+}
