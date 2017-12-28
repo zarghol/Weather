2d0e65fbe5325963df2af9ded9fbb9d912f69702
Clément NONN <clement.nonn@gmail.com>
2017-12-28 19:33:10 +0100

    add other colors to view

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     // MARK: - Outlets
     
     @IBOutlet weak var cityButton: UIButton!
+    @IBOutlet weak var sunSeparator: UIView!
+    @IBOutlet weak var sunsetImageView: UIImageView!
+    @IBOutlet weak var sunriseImageView: UIImageView!
+    @IBOutlet weak var separator: UIView!
     
     @IBOutlet weak var currentTemperatureLabel: UILabel!
     @IBOutlet weak var minTemperatureLabel: UILabel!
@@ -32,6 +36,11 @@ class ViewController: UIViewController {
     let temperatureFormatter: MeasurementFormatter = {
         let temperatureFormatter = MeasurementFormatter()
         temperatureFormatter.unitOptions = .temperatureWithoutUnit
+        let numberFormatter = NumberFormatter()
+        numberFormatter.minimumFractionDigits = 0
+        numberFormatter.maximumFractionDigits = 1
+        numberFormatter.minimumIntegerDigits = 1
+        temperatureFormatter.numberFormatter = numberFormatter
         return temperatureFormatter
     }()
     
@@ -47,34 +56,49 @@ class ViewController: UIViewController {
             }
             
             DispatchQueue.main.async {
-                self.view.backgroundColor = forecast.conditions.first?.type.backgroundColor ?? .white
+                let type = forecast.conditions.first?.type ?? WeatherType.other
+
+                self.view.backgroundColor = type.backgroundColor
                 self.cityButton.setTitle(forecast.city, for: .normal)
+                self.cityButton.tintColor = type.thirdColor
                 self.currentTemperatureLabel.text = self.temperatureFormatter.string(from: forecast.temperature)
+                self.currentTemperatureLabel.textColor = type.textColor
                 self.minTemperatureLabel.text = self.temperatureFormatter.string(from: forecast.temperatureMinimum)
+                self.minTemperatureLabel.textColor = type.textColor
                 self.maxTemperatureLabel.text = self.temperatureFormatter.string(from: forecast.temperatureMaximum)
+                self.maxTemperatureLabel.textColor = type.textColor
                 self.weatherDescriptionLabel.text = forecast.conditions.first?.description
+                self.weatherDescriptionLabel.textColor = type.textColor
                 
                 let hourFormatter = DateFormatter()
                 hourFormatter.dateStyle = .none
                 hourFormatter.timeStyle = .short
                 if let sunrise = forecast.sunrise {
                     self.sunriseLabel.text = hourFormatter.string(from: sunrise)
+                    self.sunriseLabel.textColor = type.textColor
                 }
                 if let sunset = forecast.sunset {
                     self.sunsetLabel.text = hourFormatter.string(from: sunset)
+                    self.sunsetLabel.textColor = type.textColor
                 }
                 let normalMeasureFormatter = MeasurementFormatter()
                 self.pressureLabel.text = "Pressure : \(normalMeasureFormatter.string(from: forecast.pressure))"
-                
+                self.pressureLabel.textColor = type.textColor
                 self.humidityLabel.text = "Humidity : \(forecast.humidity) %"
-                
+                self.humidityLabel.textColor = type.textColor
                 self.cloudsLabel.text = "Clouds : \(forecast.clouds) %"
-                
+                self.cloudsLabel.textColor = type.textColor
                 if let rainVolume = forecast.rainVolume {
                     self.rainLabel.text = "Rain : \(normalMeasureFormatter.string(from: rainVolume))"
                 } else {
                     self.rainLabel.text = "Rain : no data"
                 }
+                self.rainLabel.textColor = type.textColor
+                
+                self.separator.backgroundColor = type.thirdColor
+                self.sunSeparator.backgroundColor = type.thirdColor
+                self.sunsetImageView.tintColor = type.thirdColor
+                self.sunriseImageView.tintColor = type.thirdColor
             }
         }
     }
@@ -125,15 +149,18 @@ extension ViewController: UICollectionViewDataSource {
             return dequeuedCell
         }
         let data = nextForecasts[indexPath.row]
+        let type = data.conditions.first?.type ?? .other
+        
         let dayFormatter = DateFormatter()
         dayFormatter.timeStyle = .short
         cell.dayLabel.text = dayFormatter.string(from: data.date)
+        cell.dayLabel.textColor = type.textColor
         cell.minLabel.text = temperatureFormatter.string(from: data.temperatureMinimum)
+        cell.minLabel.textColor = type.textColor
         cell.maxLabel.text = temperatureFormatter.string(from: data.temperatureMaximum)
+        cell.maxLabel.textColor = type.textColor
+        cell.backgroundColor = type.backgroundColor
         
         return cell
     }
 }
-
-// formatter : if today => hour else, date + hour
-
