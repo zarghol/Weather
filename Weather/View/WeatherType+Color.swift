65e5a18c33d6aa6e4a80c26e5a337c2ced414bf9
Clément NONN <clement.nonn@gmail.com>
2017-12-30 20:26:04 +0100

    fix again colors (satisfying result)

--- Weather/View/WeatherType+Color.swift
+++ Weather/View/WeatherType+Color.swift
@@ -11,39 +11,76 @@ import UIKit
 extension WeatherType {
     var backgroundColor: UIColor {
         switch self {
-        case .thunderstorm, .drizzle:
-            return #colorLiteral(red: 0.1725490196, green: 0.2431372549, blue: 0.3137254902, alpha: 1)
+        case .thunderstorm:
+            return #colorLiteral(red: 0.2549019608, green: 0.3294117647, blue: 0.5019607843, alpha: 1)
             
         case .clear:
-            return #colorLiteral(red: 1, green: 0.9843137255, blue: 0, alpha: 1)
+            return #colorLiteral(red: 0.03921568627, green: 0.6274509804, blue: 0.968627451, alpha: 1)
             
-        case .snow, .clouds(_):
-            return #colorLiteral(red: 0.439055702, green: 0.4862026229, blue: 0.4891493056, alpha: 1)
+        case .snow:
+            return #colorLiteral(red: 0.6745098039, green: 0.7803921569, blue: 0.8823529412, alpha: 1)
             
-        case .rain:
-            return #colorLiteral(red: 0.1607843137, green: 0.5019607843, blue: 0.7254901961, alpha: 1)
+        case .clouds(.low), .drizzle:
+            return #colorLiteral(red: 0.9019607843, green: 0.9019607843, blue: 0.9019607843, alpha: 1)
+            
+        case .clouds(.medium), .rain:
+            return #colorLiteral(red: 0.7490196078, green: 0.7490196078, blue: 0.7490196078, alpha: 1)
+            
+        case .clouds(.large):
+            return #colorLiteral(red: 0.4549019608, green: 0.4549019608, blue: 0.4549019608, alpha: 1)
             
         case .atmosphere, .other:
-            return #colorLiteral(red: 0.9254901961, green: 0.9411764706, blue: 0.9450980392, alpha: 1)
+            return #colorLiteral(red: 0.662745098, green: 0.862745098, blue: 0.9725490196, alpha: 1)
         }
     }
     
     var textColor: UIColor {
-        var hue: CGFloat = 0.0
-        var saturation: CGFloat = 0.0
-        var brightness: CGFloat = 0.0
-        self.backgroundColor.getHue(&hue, saturation: &saturation, brightness: &brightness, alpha: nil)
-        let textBrightness: CGFloat = brightness < 0.5 ? 0.75 : 0.25
-        return UIColor(hue: hue, saturation: saturation, brightness: textBrightness, alpha: 1.0)
+        switch self {
+        case .thunderstorm:
+            return #colorLiteral(red: 0.6666666667, green: 0.7176470588, blue: 0.8274509804, alpha: 1)
+            
+        case .clear:
+            return #colorLiteral(red: 0.6509803922, green: 0.8705882353, blue: 1, alpha: 1)
+            
+        case .snow:
+            return #colorLiteral(red: 0.1333333333, green: 0.2549019608, blue: 0.368627451, alpha: 1)
+            
+        case .clouds(.low), .drizzle:
+            return #colorLiteral(red: 0.1411764706, green: 0.2823529412, blue: 0.3607843137, alpha: 1)
+            
+        case .clouds(.medium), .rain:
+            return #colorLiteral(red: 0.1921568627, green: 0.2274509804, blue: 0.3098039216, alpha: 1)
+            
+        case .clouds(.large):
+            return #colorLiteral(red: 0.7098039216, green: 0.7098039216, blue: 0.7098039216, alpha: 1)
+            
+        case .atmosphere, .other:
+            return #colorLiteral(red: 0.0709715337, green: 0.4961835921, blue: 0.7251439313, alpha: 1)
+        }
     }
     
     var thirdColor: UIColor {
-        var hue: CGFloat = 0.0
-        var saturation: CGFloat = 0.0
-        var brightness: CGFloat = 0.0
-        self.backgroundColor.getHue(&hue, saturation: &saturation, brightness: &brightness, alpha: nil)
-        let thirdBrightness: CGFloat = brightness <= 0.5 ? 0.65 : 0.35
-        let thirdHue: CGFloat = hue + 0.5 * (hue > 0.5 ? -1.0 : 1.0)
-        return UIColor(hue: thirdHue, saturation: saturation, brightness: thirdBrightness, alpha: 1.0)
+        switch self {
+        case .thunderstorm:
+            return #colorLiteral(red: 0.831372549, green: 0.7843137255, blue: 0.6666666667, alpha: 1)
+            
+        case .clear:
+            return #colorLiteral(red: 0.9921568627, green: 0.9607843137, blue: 0.03921568627, alpha: 1)
+            
+        case .snow:
+            return #colorLiteral(red: 0.368627451, green: 0.2470588235, blue: 0.1333333333, alpha: 1)
+            
+        case .clouds(.low), .drizzle:
+            return #colorLiteral(red: 0.2470588235, green: 0.2470588235, blue: 0.2470588235, alpha: 1)
+            
+        case .clouds(.medium), .rain:
+            return #colorLiteral(red: 0.262745098, green: 0.2235294118, blue: 0.1294117647, alpha: 1)
+            
+        case .clouds(.large):
+            return #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+            
+        case .atmosphere, .other:
+            return #colorLiteral(red: 0.6660050934, green: 0.5509921657, blue: 0.4948230614, alpha: 1)
+        }
     }
 }
