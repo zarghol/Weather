269cceab3e328d3e9d6184bfcf8bb9eaf4f17ec5
Clément NONN <clement.nonn@gmail.com>
2017-12-31 17:45:22 +0100

    add coordinates to possible location

--- Weather/Network/OpenWeatherConfiguration.swift
+++ Weather/Network/OpenWeatherConfiguration.swift
@@ -11,6 +11,7 @@ import Foundation
 enum OpenWeatherLocation {
     case id(Int)
     case query(String)
+    case coordinate(Double, Double)
 }
 
 enum OpenWeatherUnit: String {
@@ -27,22 +28,28 @@ struct OpenWeatherConfiguration {
 // MARK: - Allow to add the configuration to query of the url
 
 extension OpenWeatherLocation {
-    var queryItem: URLQueryItem {
+    var queryItem: [URLQueryItem] {
         switch self {
         case .id(let id):
-            return URLQueryItem(name: "id", value: "\(id)")
+            return [URLQueryItem(name: "id", value: "\(id)")]
             
         case .query(let str):
-            return URLQueryItem(name: "q", value: str)
+            return [URLQueryItem(name: "q", value: str)]
+            
+        case .coordinate(let lat, let long):
+            return [URLQueryItem(name: "lat", value: "\(lat)"),
+                    URLQueryItem(name: "lon", value: "\(long)")]
         }
     }
 }
 
 extension OpenWeatherConfiguration {
     var queryItems: [URLQueryItem] {
-        let units = URLQueryItem(name: "units", value: unit.rawValue)
-        let key = URLQueryItem(name: "appid", value: apiKey)
-        let lang = URLQueryItem(name: "lang", value: language)
-        return [self.location.queryItem, units, key, lang]
+        var items = [URLQueryItem]()
+        items.append(contentsOf: self.location.queryItem)
+        items.append(URLQueryItem(name: "units", value: unit.rawValue))
+        items.append(URLQueryItem(name: "appid", value: apiKey))
+        items.append(URLQueryItem(name: "lang", value: language))
+        return items
     }
 }
